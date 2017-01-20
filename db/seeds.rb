f8740279212c42f8a83f349be4f563c9bd9b211a
Haico27 <haico.wensink@gmail.com>
2017-01-20 11:37:54 +0100

    Through Association: Controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Booking.delete_all
 Photo.delete_all
 Theme.delete_all
 Room.delete_all
