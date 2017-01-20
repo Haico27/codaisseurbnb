05a7dae8820bca4c1b4ed2f1fb515b97da11bd26
Haico27 <haico.wensink@gmail.com>
2017-01-20 14:00:45 +0100

    Through Association: Views

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Profile.delete_all
 Booking.delete_all
 Photo.delete_all
 Theme.delete_all
@@ -37,7 +38,7 @@ photo7 = Photo.create(remote_image_url: "http://res.cloudinary.com/dnwkjak4p/ima
 photo8 = Photo.create(remote_image_url: "http://res.cloudinary.com/dnwkjak4p/image/upload/v1476007291/20161005-brctd_fju0e9.png")
 photo9 = Photo.create(remote_image_url: "http://res.cloudinary.com/dnwkjak4p/image/upload/v1476007291/20161005-ngwqx_erq7sf.png")
 
-Room.create(home_type: "House", room_type: "Shared", accommodate: 2, bedroom_count: 2,
+room1 = Room.create(home_type: "House", room_type: "Shared", accommodate: 2, bedroom_count: 2,
 bathroom_count: 3, listing_name: "You will never forget your stay!",
 description: "Normcore meh put a bird on it man braid mumblecore, locavore synth swag kogi
 helvetica ugh health goth twee raw denim readymade. Freegan yuccie everyday carry tote bag
@@ -48,13 +49,13 @@ unicorn.", address: "Colon 58, 1B, 11100 Cadiz (Spain)", has_tv: false, has_kitc
 has_airco: false, has_heating: false, has_internet: true, price: 100, active: true, user: miriam,
 themes: [modern_and_minimal, sleek_and_sophisticated], photos: [photo1, photo2, photo3])
 
-Room.create(home_type: "House", room_type: "Shared", accommodate: 2, bedroom_count: 2,
+room2 = Room.create(home_type: "House", room_type: "Shared", accommodate: 2, bedroom_count: 2,
 bathroom_count: 3, listing_name: "A place in the middle of Holland", description: "Freegan yuccie
 everyday carry tote bag you probably haven't heard of them godard.", address: "Daguerrestraat 23,
 1054TG Utrecht (Netherlands)", has_tv: false, has_kitchen: true, has_airco: false, has_heating: false,
 has_internet: true, price: 100, active: true, user: wouter, themes: [warm_and_cozy, a_mans_touch], photos: [photo4, photo5, photo6])
 
-Room.create(home_type: "House", room_type: "Shared", accommodate: 2, bedroom_count: 2, bathroom_count: 3,
+room3 = Room.create(home_type: "House", room_type: "Shared", accommodate: 2, bedroom_count: 2, bathroom_count: 3,
 listing_name: "Great for a Weekend", description: "Meditation tattooed lumbersexual street art truffaut
 pour-over, kale chips pitchfork. Readymade snackwave man bun, photo booth tilde wolf occupy fashion axe VHS authentic PBR&B typewriter. Vape everyday carry meditation try-hard hammock unicorn.",
 address: "Tottenham Court Road 14, W1T 1JY London (UK)", has_tv: false, has_kitchen: true,
